ab7ead3bca60a53faf77e95c3210aced622e51db
Yaroslav <onuchak.j@gmail.com>
2021-07-05 18:04:35 +0300

    test

---
+++ src/app/core/services/authentication/auth-guard.service.ts
@@ -0,0 +1,63 @@
+import {Injectable} from '@angular/core';
+import {CanActivate} from '@angular/router';
+import {ActivatedRouteSnapshot} from '@angular/router';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {map, catchError} from 'rxjs/operators';
+import {User} from "../../interfaces";
+import {environment} from "../../../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate {
+  public user: User;
+  private loginStatus: boolean = true
+
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  login(username?: string, password?: string): Observable<User> {
+    return this.http.get<any>(`${environment.apiUrl}/users`)
+      .pipe(map(
+        users => {
+         let user = users.filter((elem: User) => elem.username === username && elem.password === password)[0];
+         this.loginStatus = true
+          this.user = user;
+          return user;
+        }
+      ));
+  }
+
+  loginedUser(): User {
+    return this.user
+  }
+
+  canActivate(route: ActivatedRouteSnapshot): Observable<boolean> {
+    return this.http.get(`${environment.apiUrl}/users`).pipe(
+      map(
+        res => {
+          if (!res)
+            // if (true)
+          {
+            alert("no data");
+            return false;
+          } else {
+            if (this.loginStatus) {
+              return true;
+            }
+            return false;
+          }
+        }
+      ),
+      catchError((err) => {
+        return of(false);
+      })
+    );
+
+  }
+
+}
